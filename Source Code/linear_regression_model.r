45e135f684efc7f68ebd91223f7043b2e0a1734b
OrderBrain <matthew@Matthews-MacBook-Pro.local>
2016-12-03 19:09:54 -0500

    added linear regression model

--- Source Code/linear_regression_model.r
+++ Source Code/linear_regression_model.r
@@ -1,4 +1,4 @@
-# polynomial regression using random variable selected by me
+# linear regression using random variable selected by me
 adult.poly = lm(income~education+workclass+maritalStatus+race+sex, data=adult.data)
 
 # do the prediction
@@ -9,3 +9,4 @@ yhat = ifelse(yhat >= 0.5 , 1, 0)
 
 # get and display the mse
 mean((yhat - adult.test$income) ^ 2)
+
