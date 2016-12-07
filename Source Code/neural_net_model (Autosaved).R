dabc3d0718d842560d437ff35ec5f1a5be2d0d50
OrderBrain <matthew@wireless66-167.wireless.wmich.edu>
2016-12-07 18:52:10 -0500

    final r files

--- Source Code/neural_net_model (Autosaved).R
+++ Source Code/neural_net_model (Autosaved).R
@@ -1,17 +1,14 @@
-# Regression Tree
 library(nnet)
-adult.nnet = nnet(income~education+maritalStatus+race+sex+hoursPerWeek+occupation, data=adult.data, size=5, maxit=5000, decay=.001)
+adult.nnet = nnet(income~educationNum, data=adult.data, size=15, maxit=10000, decay=.1)
 
-#lm(income~education+workclass+maritalStatus+race+sex+poly(educationNum,8)+poly(hoursPerWeek,8), data=adult.data)
-
-summary(adult.poly)
+adult.glm = glm(income~age+education+capitalGain+capitalLoss+hoursPerWeek+sex
+        	+race+workclass+maritalStatus+relationship+occupation,data=adult.data,family= binomial)
 
 # do the prediction
-yhat = predict(adult.nnet,adult.test)
+yhat = predict(adult.glm,adult.test)
 
 # if yhat is greater than .5 it is 1 else 0
-yhat = ifelse(yhat >= 0.5 , 1, 0)
+yhat = ifelse(yhat >= 0, 1, 0)
 
 # get and display the mse
 mean((yhat - adult.test$income) ^ 2)
-
--- Source Code/neural_net_model.R
+++ Source Code/neural_net_model.R
@@ -1,13 +1,13 @@
 # Regression Tree
-library(tree)
-adult.tree = tree(income~education+workclass+maritalStatus+race+sex, data=adult.data)
+library(nnet)
+adult.nnet = nnet(income~education+maritalStatus+race+sex+hoursPerWeek+occupation, data=adult.data, size=5, maxit=5000, decay=.001)
 
 #lm(income~education+workclass+maritalStatus+race+sex+poly(educationNum,8)+poly(hoursPerWeek,8), data=adult.data)
 
-summary(adult.tree)
+summary(adult.poly)
 
 # do the prediction
-yhat = predict(adult.tree,adult.test)
+yhat = predict(adult.nnet,adult.test)
 
 # if yhat is greater than .5 it is 1 else 0
 yhat = ifelse(yhat >= 0.5 , 1, 0)
